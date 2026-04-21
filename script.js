353fbaa572550ae3070af9ca849b9a7c5c0b8d07
grcrrsl <grcrrsl@example.com>
2026-04-22 05:44:43 +0800

    initail commit

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ reveals.forEach((el) => io.observe(el));
 
 // immediately show hero
 document.querySelector(".hero .reveal").classList.add("visible");
+    const scrollBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.add("show");
+    } else {
+      scrollBtn.classList.remove("show");
+    }
+  });
+
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
